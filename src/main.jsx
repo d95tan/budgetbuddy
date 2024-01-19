04b1f7f23a2527ad7f254851f80567a197f1a7dd
Dan Desktop <d95tan@gmail.com>
2024-01-19 18:46:51 +0800

    feat: edited footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App/App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Footer } from "antd/es/layout/layout.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    <footer style={{ textAlign: "center" }}>
-      © 2024 By{" "}
-      <a id="github" href="https://github.com/d95tan/">
-        Dan
-      </a>
-    </footer>
+    <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        © {new Date().getFullYear()} By Dan Tan, Justin Teh, Raphael Lim 
+      </Footer>
   </React.StrictMode>
 );
