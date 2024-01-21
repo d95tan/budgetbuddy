1c67ee1cebeb548089819a879459fe36f5d9f6f5
justintea <justin.tehhy@gmail.com>
2024-01-22 01:28:24 +0800

    feat: loginform fields & button components

--- src/components/Auth - LoginForm/LoginForm.jsx
+++ src/components/Auth - LoginForm/LoginForm.jsx
@@ -2,7 +2,16 @@ export default function LoginForm() {
   
   return (
     <>
-    <h2> LoginForm component code here</h2>
+      <h2> Login</h2>
+      <label>Email</label>
+      <input type='email' required />
+
+      <label>Password</label>
+      <input type='password' required />
+
+      <button type='login'>Login</button>
+      <p><i>Don't have an account? Sign up now </i></p>     
+      {/* J: potentially 'Sign up now' is hyperlink to Signup component */}
     </>
 );
 }
