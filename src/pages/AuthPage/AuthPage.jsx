e601b50e052017261561667882d0d49156401e7c
justintea <justin.tehhy@gmail.com>
2024-01-21 21:12:48 +0800

    feat: Authpage references LoginForm component

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,11 @@
+import LoginForm from "../../components/Auth - LoginForm/LoginForm"
+
 export default function AuthPage() {
   return (
     <>
-      <h3>Justin: Login components here</h3>
+      <h3>Justin: Login components below </h3>
       <p><i>will aim to copy IBKR page format here</i> </p>
+      <LoginForm />
     </>
 )
 }
