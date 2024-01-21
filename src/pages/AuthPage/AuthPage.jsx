69bacbca57e24f34b10e801c02991895d6973e65
justintea <justin.tehhy@gmail.com>
2024-01-22 01:33:02 +0800

    chore: edit text

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -4,7 +4,7 @@ export default function AuthPage() {
   return (
     <>
       <h3>Justin: Login components below </h3>
-      <p><i>will aim to copy IBKR page format here</i> </p>
+      <p><i>Approach: will aim to copy IBKR page format here</i> </p>
       <LoginForm />
     </>
 )
