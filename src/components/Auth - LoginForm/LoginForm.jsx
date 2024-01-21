0dbac20bef8670acfe2b00449c24965e2a3554fd
justintea <justin.tehhy@gmail.com>
2024-01-22 01:32:22 +0800

    feat: loginform - temporary format line breaks

--- src/components/Auth - LoginForm/LoginForm.jsx
+++ src/components/Auth - LoginForm/LoginForm.jsx
@@ -1,17 +1,27 @@
 export default function LoginForm() {
-  
   return (
     <>
-      <h2> Login</h2>
-      <label>Email</label>
-      <input type='email' required />
+      <div className="form-container">
+        {/* J: i want to import styles, ask tomorrow */}
+        <form>
+          <h2> Login</h2>
+          <label>Email</label>
+          <input type="email" required />
+          <br />
+          <br />
 
-      <label>Password</label>
-      <input type='password' required />
+          <label>Password</label>
+          <input type="password" required />
+          <br />
+          <br />
 
-      <button type='login'>Login</button>
-      <p><i>Don't have an account? Sign up now </i></p>     
-      {/* J: potentially 'Sign up now' is hyperlink to Signup component */}
+          <button type="login">Login</button>
+          <p>
+            <i>Don't have an account? Sign up now </i>
+          </p>
+          {/* J: potentially 'Sign up now' is hyperlink to Signup component */}
+        </form>
+      </div>
     </>
-);
+  );
 }
