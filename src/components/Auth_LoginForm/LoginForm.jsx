56cfb88bc54ce24d75f24df40ac7d05aac9fd0cd
justintea <justin.tehhy@gmail.com>
2024-01-22 11:38:23 +0800

    feat: implement AUI library for form

---
+++ src/components/Auth_LoginForm/LoginForm.jsx
@@ -0,0 +1,105 @@
+import { Button, Checkbox, Form, Input } from 'antd';
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+export default function LoginForm() {
+  return (
+    <>
+      <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+    </>
+  );
+}
+
+//* old code 
+/* <div className="form-container">
+         J: i want to import styles, ask tomorrow 
+        <form>
+          {/* <h2> Login</h2>
+          <label>Email</label>
+          <input type="email" required />
+          <br />
+          <br />
+
+          <label>Password</label>
+          <input type="password" required />
+          <br />
+          <br />
+
+          <button type="login">Login</button>
+          <p>
+            <i>Don't have an account? Sign up now </i>
+          </p> 
+          {/* J: potentially 'Sign up now' is hyperlink to Signup component 
+        //? try AUI code
+      
+        </form>
+      </div> */
