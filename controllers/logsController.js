b8385b60f0f160b6e4c293d0560849143835b19d
Dan Desktop <d95tan@gmail.com>
2024-01-22 23:27:20 +0800

    feat: graph on dashboard, moved sorting to backend

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -4,6 +4,15 @@ const index = async (req, res) => {
   // const {userId} = req.get("Authorization").split(" ")[1]
   // const logs = await Log.find({userId})
   const logs = await Log.find({});
+  logs.sort((a, b) => {
+    const dateA = a.date;
+    const dateB = b.date;
+    if (dateA < dateB) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
   res.json(logs)
 } 
 
@@ -11,6 +20,10 @@ const create = async (req, res) => {
   const data = req.body;
   const log = await Log.create(data);
   res.json(log)
+}
+
+const updateMany = async (req, res) => {
+  const data = req.body;
 }
 
 module.exports = {
--- src/pages/UserPage/DashboardPage/DashboardPage.jsx
+++ src/pages/UserPage/DashboardPage/DashboardPage.jsx
@@ -1,12 +1,12 @@
 import { useOutletContext } from "react-router-dom";
+import Graph from "../../../components/Graphs/Graph";
 
 export default function DashboardPage() {
   const [logs, setLogs] = useOutletContext();
-
   return (
     <>
       <h1>Dashboard Page</h1>
-      <p>{JSON.stringify(logs)}</p>
+      <Graph logs={logs} />
     </>
   );
 }
