66c68635f53be67b2aaf4de155f977531ab0bcd2
justintea <justin.tehhy@gmail.com>
2024-01-24 02:43:23 +0800

    refactor: HTTP requests all in sendRequest.js now

--- src/utilities/usersAPI.js
+++ src/utilities/usersAPI.js
@@ -1,21 +1,6 @@
 const BASE_URL = '/api/users';
+import sendRequest from "./sendRequest";
 
 export async function signUp(userData) {
-
-  //? 1. HTTP request
-  const res = await fetch(BASE_URL, {
-    method: 'POST',
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userData),
-  });
-
-
-  //? 2. Check if request was successful
-  if (res.ok) {
-    return res.json();
-  }
-  else {
-    throw new Error('Invalid sign up details');
-  }
-
+  return sendRequest(BASE_URL, 'POST', userData);
 }
--- src/utilities/usersService.js
+++ src/utilities/usersService.js
@@ -3,5 +3,29 @@ import * as usersAPI from "./usersAPI";
 export async function signUp(userData) {
 
   const token = await usersAPI.signUp(userData);
-  return token; 
+  localStorage.setItem('token', token);
+
+  return getUser(); 
+}
+
+
+export function getUser() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split(".")[1])).user : null;
+}
+
+export function getToken() {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+  
+  // if have token, obtain the payload of the token
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  
+  // A JWT's exp is expressed in seconds, not milliseconds, so convert
+  if (payload.exp < Date.now() / 1000) {
+    // Token has expired - remove it from localStorage
+    localStorage.removeItem("token");
+    return null;
+  }
+  return token;
 }
