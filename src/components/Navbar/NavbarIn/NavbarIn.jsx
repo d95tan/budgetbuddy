3338b0cf485f537e57f514f51acd05561391a29f
justintea <justin.tehhy@gmail.com>
2024-01-24 23:59:37 +0800

    feat: created logout button

--- src/components/Navbar/NavbarIn/NavbarIn.jsx
+++ src/components/Navbar/NavbarIn/NavbarIn.jsx
@@ -1,11 +1,12 @@
 import "../Navbar.css";
 import { SettingOutlined } from "@ant-design/icons";
+import { PoweroffOutlined } from "@ant-design/icons";
 import { Layout, Button, Menu } from "antd";
 import { Link, useLocation } from "react-router-dom";
 const { Header } = Layout;
 import * as usersService from '../../../utilities/usersService';
 
-export default function NavbarIn( { User } ) {
+export default function NavbarIn( { setUser } ) {
   let page = useLocation().pathname.split("/")[2];
   
   const items = [
@@ -17,9 +18,7 @@ export default function NavbarIn( { User } ) {
 
   //* J 24/1 0330: temporary addition - logout button
   function handleLogOut() {
-    // Delegate to the users-service
     usersService.logOut();
-    // Update state will also cause a re-render
     setUser(null);
   }
 
@@ -40,8 +39,12 @@ export default function NavbarIn( { User } ) {
 
       
       {/* J 24/1 0330: temporary addition - logout button */}
-      <Link to="" onClick={handleLogOut}> Log Out </Link>
-
+      <div className="logout-buttons">
+        <Link to="/login" onClick={handleLogOut}>
+          <Button icon={<PoweroffOutlined />} style={{ marginRight: '5px' }}></Button>
+        </Link>
+      </div>
+      
       <div className="auth-buttons">
         <Link to="/user/preferences">
           <Button icon={<SettingOutlined />}></Button>
