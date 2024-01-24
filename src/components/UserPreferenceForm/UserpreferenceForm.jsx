1b9ab02f8998f6f8ccb5ce59274a7c569dcf9fa9
justintea <justin.tehhy@gmail.com>
2024-01-25 01:09:30 +0800

    feat: major styling done on Preference form

--- src/components/UserPreferenceForm/UserpreferenceForm.jsx
+++ src/components/UserPreferenceForm/UserpreferenceForm.jsx
@@ -1,38 +1,93 @@
 import React, { useEffect, useState } from 'react';
-import { Avatar, List , Modal } from 'antd';
+import { Avatar, List, Modal } from 'antd';
+import { HistoryOutlined , StockOutlined , CalendarOutlined} from "@ant-design/icons";
 import OurDatePicker from './DatePicker';
 
-export default function UserPreferenceForm() {
+export default function UserPreferenceForm() {   //? originally from AntD
+
+  //? improved for our use
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
+  const [currentModalContent, setCurrentModalContent] = useState(false);
+
+  const showModal = (modalContent) => {
     setIsModalOpen(true);
+    setCurrentModalContent(modalContent);
   };
   const handleOk = () => {
     setIsModalOpen(false);
+    setCurrentModalContent(false);
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+    setCurrentModalContent(false);
   };
 
-  const data = [
+  //? originally from AntD
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  //
+  // const showModal = () => {
+  //   setIsModalOpen(true);
+  // };
+  // const handleOk = () => {
+  //   setIsModalOpen(false);
+  // };
+  // const handleCancel = () => {
+  //   setIsModalOpen(false);
+  // };
+  //
+  // return (
+  //   <>
+  //     <Button onClick={showModal}>  Open Modal   </Button>
+  //     <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+  //       <p>Some contents...</p>     </Modal>
+  //   </>   );
+
+
+  const preferlist = [
     {
       title: "Update frequency",
       description: "How often would you like to input financial information?",
+      icon: <HistoryOutlined style={{ fontSize: "200%" }} />,
+      modalContent: (
+        <div>
+          <h2>Update frequency</h2>
+          <p>Custom content for Update frequency</p>
+          <label>freq</label>
+          <input />
+        </div>
+      ),
     },
     {
       title: "Current income",
       description:
         "How much do you earn monthly? Note: This is strictly used for your computation purposes.",
+      icon: <StockOutlined style={{ fontSize: "200%" }} />,
+      modalContent: (
+        <div>
+          <h2>Current income</h2>
+          <p>Custom content for Current income</p>
+          <label>income</label>
+          <input />
+
+        </div>
+      ),
     },
     {
       title: "Birthday",
       description:
         "This will be used for your self-declared financial goal planning.",
+      icon: <CalendarOutlined style={{ fontSize: "200%" }} />,
+      modalContent: (
+        <div>
+          <h2>Update frequency</h2>
+          <p>Custom content for Update frequency</p>
+          <OurDatePicker />
+        </div>
+      ),
     },
   ];
 
   // i want to render it upon coming in
-
   // i want to implement modal window
   // i want to update the account details
 
@@ -40,36 +95,32 @@ export default function UserPreferenceForm() {
     <>
       <List
         itemLayout="horizontal"
-        dataSource={data}
+        dataSource={preferlist}
         renderItem={(item, index) => (
-          <List.Item onClick={showModal}>
+          // <List.Item onClick={showModal(item.modalContent)}>
+          <List.Item onClick={() => showModal(item.modalContent)}>
+
             <List.Item.Meta
-              avatar={
-                <Avatar
-                  src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
-                />
-              }
-              // title={<a href="https://ant.design">{item.title}</a>}
+              avatar={item.icon}
               title={item.title}
               description={item.description}
             />
 
-            <div>Content</div>
+            {/* // this part to render value */}
+            <div>Content</div>        
           </List.Item>
         )}
       />
 
-      <Modal
-        title="Basic Modal"
+        <Modal
+        // title="Basic Modal"
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <OurDatePicker />
+        {currentModalContent}
       </Modal>
+
     </>
   );
 }
