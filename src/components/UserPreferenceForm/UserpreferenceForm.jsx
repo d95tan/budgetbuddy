46491c22fd709ad23e50d6cf349107c026340aa3
justintea <justin.tehhy@gmail.com>
2024-01-24 19:17:21 +0800

    feat: created Datepicker, within modal window, to set Birthday

--- src/components/UserPreferenceForm/UserpreferenceForm.jsx
+++ src/components/UserPreferenceForm/UserpreferenceForm.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar, List , Modal } from 'antd';
+import OurDatePicker from './DatePicker';
 
 export default function UserPreferenceForm() {
-  
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
@@ -16,49 +16,62 @@ export default function UserPreferenceForm() {
 
   const data = [
     {
-      title: 'Update frequency', description: 'How often would you like to input financial information?',
+      title: "Update frequency",
+      description: "How often would you like to input financial information?",
     },
     {
-      title: 'Current income', description: 'How much do you earn monthly? Note: This is strictly used for your computation purposes.'
+      title: "Current income",
+      description:
+        "How much do you earn monthly? Note: This is strictly used for your computation purposes.",
     },
     {
-      title: 'Birthday', description: 'This will be used for your self-declared financial goal planning.'
+      title: "Birthday",
+      description:
+        "This will be used for your self-declared financial goal planning.",
     },
   ];
 
-  // i want to render it upon coming in 
+  // i want to render it upon coming in
 
   // i want to implement modal window
   // i want to update the account details
 
+  return (
+    <>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item, index) => (
+          <List.Item onClick={showModal}>
+            <List.Item.Meta
+              avatar={
+                <Avatar
+                  src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+                />
+              }
+              // title={<a href="https://ant.design">{item.title}</a>}
+              title={item.title}
+              description={item.description}
+            />
 
-  return (<>
-  <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item, index) => (
-      <List.Item onClick={ showModal }>
-        <List.Item.Meta
-          avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-          // title={<a href="https://ant.design">{item.title}</a>}
-          title={item.title}
+            <div>Content</div>
+          </List.Item>
+        )}
+      />
 
-          description={item.description} 
-        />
-       
-        
-        <div>Content</div>
-      </List.Item>
-      )}
-    />
-    
-   <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
         <p>Some contents...</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
-        </Modal>
-  </>);
-  
+        <OurDatePicker />
+      </Modal>
+    </>
+  );
 }
   
 
