848bf1e4684ba2ed5bffa953dbe5d6cf0e4e5317
Dan Desktop <d95tan@gmail.com>
2024-01-24 17:13:04 +0800

    refactor: routing and user state

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -9,43 +9,37 @@ import EditPage from "../UserPage/EditPage/EditPage";
 import GoalsPage from "../UserPage/GoalsPage/GoalsPage"
 import AuthPage from "../AuthPage/AuthPage";
 import SignupPage from "../SignupPage/SignupPage";
-import NavbarIn from "../../components/Navbar/NavbarIn/NavbarIn";
 
 function App() {
   //*J,21/1,2100: State variable & setter function declaration 
   const [user, setUser] = useState(null);
   // console.log(user);
 
-  //* J,21/1,2100: holding code for Login page
-  if (user === null) {
-    return (
-      <>
-        <Routes>
-        <Route path="/" element={<LandingPage  />} />
-          <Route path="/login" element={<AuthPage setUser={setUser} />} />
-          <Route path="/signup" element={<SignupPage setUser={setUser}/>} />
-
-        </Routes>
-      </>
-    );
-  } 
-
-  return (
+  return (<>
+    { user ? (
+        <>
+          <Routes>
+          <Route path="/" element={<UserPage user={user} setUser={setUser} />} />
+            <Route path="user" element={<UserPage />}>
+              <Route path="dashboard" element={<DashboardPage />} />
+              <Route path="tracking" element={<TrackingPage />} />
+              <Route path="edit" element={<EditPage />} />
+              <Route path="goals" element={<GoalsPage />} />
+              {/* <Route path="preferences" element={<PreferencesPage />} /> */}
+            </Route>
+          </Routes>
+        </>
+        ) : (
     <>
-      {/* //? J 24/1 0120: added NavBarIn here. See UserPage.jsx */}
-      <NavbarIn user={user} setUser={setUser}/>
-      <Routes>
-        {/* <Route path="/" element={<LandingPage  />} /> */}
-        <Route path="user" element={<UserPage />}>
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="tracking" element={<TrackingPage />} />
-          <Route path="edit" element={<EditPage />} />
-          <Route path="goals" element={<GoalsPage />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage  />} />
+        <Route path="login" element={<AuthPage setUser={setUser} />} />
+        <Route path="signup" element={<SignupPage setUser={setUser}/>} />
+    </Routes>
+      </>)}
+      </>
   );
-}
+    }
 
 export default App;
 
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -3,21 +3,21 @@ import NavbarIn from "../../components/Navbar/NavbarIn/NavbarIn";
 import { useEffect, useState } from "react";
 import { getLogs } from "../../utilities/logsService";
 
-export default function UserPage() {
+export default function UserPage({ user, setUser }) {
   const [logs, setLogs] = useState([]);
 
   useEffect(() => {
     (async function () {
       const data = await getLogs();
       setLogs(data);
-      console.log(data)
+      console.log(data);
     })();
   }, []);
 
   return (
     <>
       {/* //? J 24/1 0120: remove NavBarIn here, to follow notes, to have it outside <Routes>. will place it in App.jsx. */}
-      {/* <NavbarIn /> */}
+      <NavbarIn user={user} setUser={setUser} />
       <h1>User Page</h1>
       <Outlet context={[logs, setLogs]} />
     </>
