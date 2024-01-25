709c853bc31afbc936103f9c3300c0c2699b94ba
Dan Desktop <d95tan@gmail.com>
2024-01-25 23:06:00 +0800

    feat: fixed CRUD for goals

--- src/utilities/goalsAPI.js
+++ src/utilities/goalsAPI.js
@@ -20,15 +20,45 @@ export async function getGoals() {
   return await response.json();
 }
 
+export async function createGoal(body) {
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+  };
+
+  const response = await fetch(baseURL, options);
+  if (!response.ok) throw new Error("Network response was not ok.");
+  return await response.json();
+}
+
 export async function updateGoal(body) {
   const id = body.id;
 
   const options = {
     method: "PUT",
     headers,
+    body: JSON.stringify(body),
   };
 
   const response = await fetch(`${baseURL}/${id}`, options);
+
+  if (!response.ok) throw new Error("Network response was not ok.");
+
+  const json = await response.json();
+
+  return json;
+}
+
+export async function deleteGoal(id) {
+  const options = {
+    method: "DELETE",
+    headers,
+  };
+
+  const response = await fetch(`${baseURL}/${id}`, options);
+
+  if (!response.ok) throw new Error("Network response was not ok.");
 
   const json = await response.json();
 
