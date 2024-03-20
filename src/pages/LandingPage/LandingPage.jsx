11f08c0627a408467a3a6bfaa4e74103d49cc654
Dan Desktop <d95tan@gmail.com>
2024-03-20 15:36:34 +0800

    feat: updated landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,44 @@
 // import NavbarIn from "../../components/Navbar/NavbarIn/NavbarIn"
-import NavbarOut from "../../components/Navbar/NavbarOut/NavbarOut"
-import AuthPage from "../AuthPage/AuthPage"
+import NavbarOut from "../../components/Navbar/NavbarOut/NavbarOut";
+import { Button, Typography } from "antd";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
+const { Title } = Typography
+
 
 export default function LandingPage() {
   return (
     <>
-      {/* <NavbarIn /> */}
-      {/* //? J 24/1 0050: comment out in-Navbar, as this is prelogin env */}
-  
       <NavbarOut />
-      <h1>Landing Page</h1>
-      
-    </>
-  )
-}
+      <div className="landing-page-div gradient-background">
+        <div className="landing-page-text">
+          <Title>Welcome to BudgetBuddy!</Title>
+          <Title level={5}>
+            Track your finances, set goals, <br /> and grow your wealth!
+          </Title>
+          <Link to="/login">
+            <Button type="primary">Try for free today!</Button>
+          </Link>
+        </div>
+        <img
+          className="landing-page-first-img"
+          src="Landing Page Main Graphic.png"
+        />
+      </div>
 
+      <div className="landing-page-div">
+        <img
+          className="landing-page-edit-img"
+          src="Landing Page Graphic 2.png"
+        />
+        <div className="landing-page-text">
+          <Title level={2}>
+            View detailed breakdowns <br />
+            Analyse your savings and investments
+          </Title>
+          <Title level={5}>Create goals to meet your needs.</Title>
+        </div>
+      </div>
+    </>
+  );
+}
